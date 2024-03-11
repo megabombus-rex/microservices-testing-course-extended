b035d5ac80fd486becfac0827c7d6101483902c9
JANC_PC\czech <czechumennakompie@gmail.com>
2024-03-11 19:54:08 +0100

    Add base Coupon API project

---
+++ ShoppingProject/ShoppingProject.Coupon.Database/Contexts/CouponDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingProject.Coupon.Database.Entities;
+
+namespace ShoppingProject.Coupon.Database.Contexts
+{
+    public class CouponDbContext : DbContext
+    {
+        public CouponDbContext(DbContextOptions<CouponDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Entities.Coupon> Coupons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ShoppingProject/ShoppingProject.Coupon.Database/Entities/Coupon.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingProject.Coupon.Database.Entities
+{
+    public class Coupon
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        public double MinAmount { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
